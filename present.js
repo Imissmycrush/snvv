59a5d3c03c897b799e0a94da1ba108f9d28409b6
Imissmycrush <nguyentrongv333@gmail.com>
2024-08-28 20:35:03 +0700

    Update present.js

--- present.js
+++ present.js
@@ -34,7 +34,7 @@ function init() {
   }
     
   present.addEventListener("click", function(e){
-    present.classList.toggle("open");
+    present.classList.toggle("none");
     document.getElementById('card').classList.add('card-show');
   }, false);
   
