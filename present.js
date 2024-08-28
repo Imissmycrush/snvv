eda277124d4a0c786837eb6247280657ee83bbd0
Imissmycrush <nguyentrongv333@gmail.com>
2024-08-28 23:18:33 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,3 +1,5 @@
+
+
 var to = nameGirl;
 var gift_url = giftUrl;
 var gift_image_url = giftImage || giftImageBase64;
@@ -8,59 +10,39 @@ var presentImage = document.getElementById("present-image");
 
 function init() {
   var graphElem = document.querySelector('.present-box > .side.top .to');
-  if (graphElem) {
-    graphElem.setAttribute('data-before', eventName);
-  }
-  
-  var cardTitle = document.querySelector('#card .title-card');
-  if (cardTitle) {
-    cardTitle.innerHTML = 💘${titleCard}💘;
-  }
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
 
-  var cardContent = document.querySelector('#card .content-card');
-  if (cardContent) {
-    cardContent.innerHTML = contentCard;
-  }
-  
-  var honeyImage = document.querySelector('#card .honey');
-  if (honeyImage) {
-    honeyImage.setAttribute('src', giftImage);
-  }
-
-  var _giftLink, _giftImg;
+  var _giftLink, 
+      _giftImg;
   
   if (gift_url) {
     _giftLink = document.createElement("a");
     _giftLink.href = gift_url;
     _giftLink.target = "_blank";
-    if (presentImage) {
-      presentImage.appendChild(_giftLink);
-    }
+    presentImage.appendChild(_giftLink);
   }
   
   if (gift_image_url) {
     _giftImg = document.createElement("img");
     _giftImg.src = gift_image_url;
-    if (_giftLink) {
+    if(_giftLink) {
       _giftLink.appendChild(_giftImg);
-    } else if (presentImage) {
+    } else {
       presentImage.appendChild(_giftImg);
     }
   }
     
-  if (present) {
-    present.addEventListener("click", function(e){
-      present.classList.toggle("open");
-      var card = document.getElementById('card');
-      if (card) {
-        card.classList.add('card-show');
-      }
-    }, false);
-  }
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
   
-  if (nametag) {
-    nametag.innerText = to;
-  }
+  
+  
+  nametag.innerText = to;
 }
 
 init();
