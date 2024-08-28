c1b5882841b058e6a6c19ae4d520a5e58fd30365
Imissmycrush <nguyentrongv333@gmail.com>
2024-08-28 16:52:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-const nameGirl = 'Ngọc';
-const giftUrl = 'https://www.facebook.com/profile.php?id=100077663223199';
+const nameGirl = 'Thanh Thảo';
+const giftUrl = 'https://www.facebook.com/reel/1833374437112119';
 const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Tặng báo con';
+const titleCard = 'Tặng bạch nguyệt quang của mình';
 const contentCard = 'Tin nhắn bạn đang đọc không có chất béo, không cholesterol và không chứa chất gây nghiện, nó có nhiều đường mà thôi. Dù cho có nhiều đường thế nào đi chăng nữa nó cũng không bao giờ có thể ngọt ngào như bạn – người đang đọc nó.';
 
 
-const giftImage = 'ngoc.jpg';
+const giftImage = 'TT EVOL.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
